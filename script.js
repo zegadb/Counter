b82f0e9b569ffc58cbe768302708789d13b07a91
zegadb <114694520+zegadb@users.noreply.github.com>
2022-11-13 09:54:39 +0400

    ...

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('input').forEach(item => 
+{
+    item.addEventListener('input', (event) =>
+    {
+        const num1 = document.querySelector('#first').value
+        const num2 = document.querySelector('#second').value
+        const num3 = document.querySelector('#third').value
+        const num4 = document.querySelector('#fourth').value
+        const num5 = document.querySelector('#fifth').value
+        const num1M = document.querySelector('#firstM').value
+        const num2M = document.querySelector('#secondM').value
+        const num3M = document.querySelector('#thirdM').value
+        const num4M = document.querySelector('#fourthM').value
+        const num5M = document.querySelector('#fifthM').value
+        document.querySelector('p').textContent = num1*num1M + num2*num2M + num3*num3M + num4*num4M + num5*num5M
+    })
+})
