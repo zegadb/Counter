ef7019e78a6dae8cfef7d655f0fbac4884a1357c
zegadb <zeygammehtiev@gmail.com>
2022-11-23 09:37:28 +0400

    done

--- script.js
+++ script.js
@@ -3,13 +3,33 @@ document.querySelector('#second').value = +window.localStorage.getItem('second')
 document.querySelector('#third').value = +window.localStorage.getItem('third')
 document.querySelector('#fourth').value = +window.localStorage.getItem('fourth')
 document.querySelector('#fifth').value = +window.localStorage.getItem('fifth')
+document.querySelector('#firstM').value = +window.localStorage.getItem('firstM')
+document.querySelector('#secondM').value = +window.localStorage.getItem('secondM')
+document.querySelector('#thirdM').value = +window.localStorage.getItem('thirdM')
+document.querySelector('#fourthM').value = +window.localStorage.getItem('fourthM')
+document.querySelector('#fifthM').value = +window.localStorage.getItem('fifthM')
+document.querySelector('select').value = window.localStorage.getItem('select')
+
 let summary = document.querySelector('p')
 summary.textContent = +window.localStorage.getItem('sum')
+
+{/* <option>Суры</option>
+<option>Пункты</option>
+<option>Балад</option>
+<option>13 и 14</option>
+<option>Шамс</option>
+<option>Багара</option>
+<option>Тарик</option>
+<option>Хумаза</option> */}
 
 const punkti = [20, 20, 40, 20, 20]
 const suri = [20, 20, 40, 20, 25]
 const balad = [35, 28, 45, 20, 25]
 const p13 = [25, 28, 40, 20, 20]
+const sams = [25, 28, 40, 20, 25]
+const bagara = [25, 28, 40, 20, 25]
+const tarik = [25, 28, 40, 20, 25]
+const humaza = [25, 20, 40, 20, 25]
 let sum
 
 function sumAll() {
@@ -34,7 +54,13 @@ function saveAll () {
     window.localStorage.setItem('third', document.querySelector('#third').value)
     window.localStorage.setItem('fourth', document.querySelector('#fourth').value)
     window.localStorage.setItem('fifth', document.querySelector('#fifth').value)
+    window.localStorage.setItem('firstM', document.querySelector('#firstM').value)
+    window.localStorage.setItem('secondM', document.querySelector('#secondM').value)
+    window.localStorage.setItem('thirdM', document.querySelector('#thirdM').value)
+    window.localStorage.setItem('fourthM', document.querySelector('#fourthM').value)
+    window.localStorage.setItem('fifthM', document.querySelector('#fifthM').value)
     window.localStorage.setItem('sum', document.querySelector('p').textContent)
+    window.localStorage.setItem('select', document.querySelector('select').value)
 }
 
 function clearAll () {window.localStorage.clear()}
@@ -45,6 +71,10 @@ document.querySelector('select').addEventListener('change', (event) => {
         else if (event.target.value == 'Пункты') item.value = punkti[index];
         else if (event.target.value == 'Балад') item.value = balad[index];
         else if (event.target.value == '13 пункт') item.value = p13[index];
+        else if (event.target.value == 'Шамс') item.value = sams[index];
+        else if (event.target.value == 'Хумаза') item.value = humaza[index];
+        else if (event.target.value == 'Тарик') item.value = tarik[index];
+        else if (event.target.value == 'Багара') item.value = bagara[index];
         sumAll()
     })
 })
